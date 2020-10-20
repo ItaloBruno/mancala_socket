5662cfc83e4e4fa69e5bb9093469f96063cae2b2
ItaloBruno <italobruno_207@hotmail.com>
2020-10-19 22:49:13 -0300

    :man_facepalming: refazendo todo o cliente e servidor definidos na comunicação :man

--- comunicacao/cliente.py
+++ comunicacao/cliente.py
@@ -1,59 +1,102 @@
 import socket
+import errno
 import sys
-import _thread
+from threading import Thread
 
 
-endereco_ip = sys.argv[1]
-porta = int(sys.argv[2])
-id_cliente = int(sys.argv[3])
-conexoes_ativas = []
-
-
-def enviar_mensagem_ao_servidor():
+def send_msg():
     while True:
-        messagem = input("\ndigite uma mensagem: ")
-        print(f"Enviando: {messagem}")
-        conexao.send(messagem.encode())
+        # print("iniciando o envio de mensagem")
+        try:
+            # Wait for user to input a message
+            message = input(f'{my_username} > ')
+            print("vou mandar isso que tu notou em cima,eim")
+        except KeyboardInterrupt:
+            sys.exit("\nChat encerrado!")
+
+        # If message is not empty - send it
+        if message:
+            # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+            message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + message)
+            print("enviei, viiiiiu\n\n")
 
 
-def receber_mensagem_do_servidor():
+def receber():
     while True:
         try:
-            # recebendo mensagem do servidor
-            dados = conexao.recv(2048)
-        except:
-            print("A conexão com o servidor foi encerrada!")
-            _thread.interrupt_main()
-            break
+        # Now we want to loop over received messages (there might be more than one) and print them
 
-        if not dados:
-            print("A conexão com o servidor foi encerrada!")
-            _thread.interrupt_main()
-            break
+            # Receive our "header" containing username length, it's size is defined and constant
+            username_header = client_socket.recv(HEADER_LENGTH)
+            print("\n\nrecebi alguma coisa do servidor...")
 
-        else:
-            print(f"Mensagem recebida do servidor: {dados.decode()}")
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
 
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
 
-def criar_cliente(host='localhost', port=8080):
-    # Criando um socket TCP/IP
-    conexa_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
 
-    # Realizando mapeamento entre o socket e a porta
-    infos_servidor = (host, port)
+            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
 
-    print(f"Conectando no host {infos_servidor[0]} na porta {infos_servidor[1]}")
-    conexa_cliente.connect(infos_servidor)
-    print("Conectado!")
+            # Print message
+            print(f'{username} > {message}\n\n{my_username} >')
 
-    return conexa_cliente
+        except IOError as e:
+            # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+            # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+            # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+            # If we got different error code - something happened
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print('Reading error: {}'.format(str(e)))
+                sys.exit()
 
+            # We just did not receive anything
+            continue
+
+        except Exception as e:
+            # Any other exception - something happened, exit
+            print('Reading error: '.format(str(e)))
+            sys.exit()
 
 if __name__ == '__main__':
-    conexao = criar_cliente(host=endereco_ip, port=porta)
 
-    _thread.start_new_thread(enviar_mensagem_ao_servidor, ())
-    _thread.start_new_thread(receber_mensagem_do_servidor, ())
+    HEADER_LENGTH = 10
+
+    IP = "127.0.0.1"
+    PORT = 5000
+    my_username = input("Username: ")
+
+    # Create a socket
+    # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+    # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to a given ip and port
+    client_socket.connect((IP, PORT))
+
+    # Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+    client_socket.setblocking(False)
+
+    # Prepare username and header and send them
+    # We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+    username = my_username.encode('utf-8')
+    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(username_header + username)
+
+    thread_maldita = Thread(target=send_msg)
+    thread_maldita.start()
+    thread_receber = Thread(target=receber)
+    thread_receber.start()
 
     while True:
         continue
--- comunicacao/servidor.py
+++ comunicacao/servidor.py
@@ -1,91 +1,149 @@
-from socket import socket, AF_INET, SOCK_STREAM
-from select import select
-from typing import List
+import socket
+import select
 import sys
 
-
-def enviar_mensagem_para_todos(conexao, servidor, lista_conexoes: List, mensagem: bytes):
-    for socket_c in lista_conexoes:
-    #     if socket != servidor and socket != conexao:
-        socket_c.send(mensagem)
+if len(sys.argv) != 3:
+    print("uso correto: python comunicacao/servidor.py <endereço ip> <numero da porta>")
+    exit()
 
 
-def criar_nova_conexao_cliente(servidor, conexoes):
-    print("Criando nova conexão cliente/servidor")
-    novo_cliente, infos_endereco_porta_cliente = servidor.accept()
-    conexoes.append(novo_cliente)
-    print(
-        f"Novo cliente conectado com endereço {infos_endereco_porta_cliente[0]} e porta {infos_endereco_porta_cliente[1]}!"
-    )
-    # enviar_mensagem_para_todos(novo_cliente, "conectou".encode())
+TAMANHO_MAX_MSG = 4096
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
 
+HEADER_LENGTH = 10
 
-def remover_cliente_off(conexao, lista_conexoes):
-    enviar_mensagem_para_todos(conexao, "Cliente está off".encode())
-    print("cliente está off")
-    conexao.close()
-    lista_conexoes.remove(conexao)
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# SO_ - socket option
+# SOL_ - socket option level
+# Sets REUSEADDR (as a socket option) to 1 on socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def ler_mensagem():
-    pass
+# Bind, so server informs operating system that it's going to use given IP and port
+# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
+server_socket.bind((IP, PORT))
 
+# This makes server listen to new connections
+server_socket.listen()
 
-def criar_servidor(endereco="localhost", porta=8080):
-    # Criando um socket TCP/IP
-    # AF_INET abrange os endereços do tipo IPv4
-    # SOCK_STREAM (socket de fluxo)
-    servidor_socket = socket(AF_INET, SOCK_STREAM)
+# List of sockets for select.select()
+sockets_list = [server_socket]
 
-    # Realizando mapeamento entre o socket e a porta
-    endereco_servidor = (endereco, porta)
+# List of connected clients - socket as a key, user header and name as data
+clients = {}
 
-    # Habilitando o reuso de endereço/porta
-    # servidor_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print(f'Listening for connections on {IP}:{PORT}...')
 
-    print(
-        f"Iniciando servidor...\nEscutando no endereço: {endereco_servidor[0]}, porta: {endereco_servidor[1]}"
-    )
-    servidor_socket.bind(endereco_servidor)
+# Handles message receiving
+def receive_message(client_socket):
 
-    # Limitando o máximo de conexões simultâneas que podem ser lidas, criando uma fila
-    servidor_socket.listen(10)
+    try:
 
-    return servidor_socket
+        # Receive our "header" containing message length, it's size is defined and constant
+        message_header = client_socket.recv(HEADER_LENGTH)
 
+        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        if not len(message_header):
+            return False
 
-def inicializa_servidor(servidor):
-    lista_conexoes_ativas.append(servidor)
+        # Convert header to int value
+        message_length = int(message_header.decode('utf-8').strip())
 
-    print("Servidor iniciado!")
+        # Return an object of message header and message data
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
 
-    while True:
+    except:
 
-        conexoes_leitura, conexoes_escrita, erros = select(
-            lista_conexoes_ativas, [], []
-        )
+        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
+        # or just lost his connection
+        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
+        # and that's also a cause when we receive an empty message
+        return False
 
-        for conexao in conexoes_leitura:
-            if conexao == servidor_socket:
-                criar_nova_conexao_cliente(servidor=servidor_socket, conexoes=lista_conexoes_ativas)
+while True:
 
-            else:
-                try:
-                    dados = conexao.recv(tamanho_max_dados)
-                except:
-                    remover_cliente_off(conexao, lista_conexoes_ativas)
-                    continue
+    # Calls Unix select() system call or Windows select() WinSock call with three parameters:
+    #   - rlist - sockets to be monitored for incoming data
+    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
+    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
+    # Returns lists:
+    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
+    #   - writing - sockets ready for data to be send thru them
+    #   - errors  - sockets with some exceptions
+    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
+    try:
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    except KeyboardInterrupt:
+        sys.exit("\nChat encerrado!")
 
-                if dados:
-                    enviar_mensagem_para_todos(conexao, servidor_socket, lista_conexoes_ativas, dados)
-                    print(dados.decode("utf-8"))
+    # Iterate over notified sockets
+    for notified_socket in read_sockets:
 
+        # If notified socket is a server socket - new connection, accept it
+        if notified_socket == server_socket:
 
-if __name__ == "__main__":
-    endereco_ip = sys.argv[1]
-    porta_servidor = int(sys.argv[2])
-    tamanho_max_dados: int = 4096
-    lista_conexoes_ativas = []
+            # Accept new connection
+            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
+            # The other returned object is ip/port set
+            client_socket, client_address = server_socket.accept()
 
-    servidor_socket = criar_servidor(endereco=endereco_ip, porta=porta_servidor)
-    inicializa_servidor(servidor=servidor_socket)
+            # Client should send his name right away, receive it
+            user = receive_message(client_socket)
+
+            # If False - client disconnected before he sent his name
+            if user is False:
+                continue
+
+            # Add accepted socket to select.select() list
+            sockets_list.append(client_socket)
+
+            # Also save username and username header
+            clients[client_socket] = user
+
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        # Else existing socket is sending a message
+        else:
+
+            # Receive message
+            message = receive_message(notified_socket)
+
+            # If False, client disconnected, cleanup
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                # Remove from list for socket.socket()
+                sockets_list.remove(notified_socket)
+
+                # Remove from our list of users
+                del clients[notified_socket]
+
+                continue
+
+            # Get user by notified socket, so we will know who sent the message
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            # Iterate over connected clients and broadcast message
+            for client_socket in clients:
+
+                # But don't sent it to sender
+                if client_socket != notified_socket:
+
+                    # Send user and message (both with their headers)
+                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    # It's not really necessary to have this, but will handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+
+        # Remove from list for socket.socket()
+        sockets_list.remove(notified_socket)
+
+        # Remove from our list of users
+        del clients[notified_socket]
